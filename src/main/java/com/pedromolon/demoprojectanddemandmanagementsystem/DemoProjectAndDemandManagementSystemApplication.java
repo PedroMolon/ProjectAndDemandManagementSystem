35c66506c2551fa29af77e91c97df8ce24a5c19d
PedroMolon <pedroh.molonr@gmail.com>
2025-10-30 10:05:19 -0300

    first commit

---
+++ src/main/java/com/pedromolon/demoprojectanddemandmanagementsystem/DemoProjectAndDemandManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pedromolon.demoprojectanddemandmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectAndDemandManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectAndDemandManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedromolon/demoprojectanddemandmanagementsystem/DemoProjectAndDemandManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromolon.demoprojectanddemandmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectAndDemandManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
